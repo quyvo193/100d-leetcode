8a3cc86598d35a6dbd301a6e8581624d878a9026
Quy Vo <quyvo@192.168.1.5.non-exists.ptr.local>
2025-08-23 11:49:23 +0700

    3sum

---
+++ js/3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const result = [];
+
+  nums = Array.from(new Set(nums));
+  if (nums.length < 3) {
+    nums = [0, 0, 0];
+  }
+
+  console.log(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+    let x = i;
+    let y = i + 1;
+    let z = i + 2;
+
+    while (y < nums.length - 1) {
+      while (z < nums.length) {
+        if (nums[x] + nums[y] + nums[z] === 0) {
+          result.push([nums[x], nums[y], nums[z]]);
+        }
+        z++;
+      }
+
+      y++;
+      z = y + 1;
+    }
+
+    return result;
+  }
+};
