4fdd67ef1b86fd6e99b57a890c0443f102267356
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-08-26 23:45:41 +0700

    top k frequent elements done

--- medium/js/top-k-frequent-elements.js
+++ medium/js/top-k-frequent-elements.js
@@ -9,15 +9,19 @@ var topKFrequent = function (nums, k) {
   const result = [];
 
   for (let n of nums) {
-    freqMap.set((freqMap.get(n) || 0) + 1);
+    freqMap.set(n, (freqMap.get(n) || 0) + 1);
   }
 
   for (let [num, freq] of freqMap) {
     bucket[freq] = (bucket[freq] || new Set()).add(num);
   }
 
-  console.log(freqMap.values());
-  console.log(bucket.map((item) => item));
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) result.push(...bucket[i]);
+    if (result.length === k) break;
+  }
+
+  return result;
 };
 
-topKFrequent([1, 1, 1, 2, 2, 3], 2);
+console.log(topKFrequent([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], 2));
