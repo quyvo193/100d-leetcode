a6a518942f943a58aa08533a1b28b47dab782ca6
Quy Vo <quyvo@Quys-Air.fpt>
2025-08-27 16:11:29 +0700

    product of array except self done

--- medium/js/product-of-array-except-self.js
+++ medium/js/product-of-array-except-self.js
@@ -2,4 +2,33 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function (nums) {};
+var productExceptSelf = function (nums) {
+  let indexZero = [];
+
+  nums.forEach((num, i) => {
+    if (num === 0) indexZero.push(i);
+  });
+
+  if (indexZero.length > 1) {
+    return Array.from({ length: nums.length }).fill(0);
+  }
+
+  if (indexZero.length === 1) {
+    const result = Array.from({ length: nums.length }).fill(0);
+
+    result[indexZero[0]] = nums.reduce((p, num, i) => {
+      if (i === indexZero[0]) {
+        return p;
+      }
+      return p * num;
+    }, 1);
+
+    return result;
+  }
+
+  const productOfAll = nums.reduce((p, num) => p * num, 1);
+
+  return nums.map((num) => productOfAll / num);
+};
+
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
