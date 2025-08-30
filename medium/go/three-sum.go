06c154d4ecbbe9d670e1dd20a576ffc6ec5e9e47
Quy Vo <quyvo@192.168.1.48>
2025-08-30 18:02:39 +0700

    three sum go continue

--- medium/go/three-sum.go
+++ medium/go/three-sum.go
@@ -8,10 +8,38 @@ import (
 func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
 
-	return [][]int{}
+	res := [][]int{}
+
+	for i := range len(nums) {
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
+		}
+
+		l, r := i+1, len(nums)-1
+
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+			} else if sum > 0 {
+				l++
+				for nums[l] == nums[l-1] {
+					l++
+				}
+			} else {
+				r--
+			}
+
+		}
+	}
+
+	return res
 }
 
 func main() {
-	r := twoSum([]int{2, 3, 4}, 6)
-	fmt.Printf("R: %v\n", r)
+	a := []int{}
+
+	a = append(a, 5)
+	// r := twoSum([]int{2, 3, 4}, 6)
+	fmt.Printf("R: %v\n", a)
 }
