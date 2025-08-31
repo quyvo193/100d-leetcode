7aba43c33de17ad9c55386220ca8c9f80edf17a4
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-01 04:39:19 +0700

    trapping-rain-water.go done

--- medium/go/trapping-rain-water.go
+++ medium/go/trapping-rain-water.go
@@ -4,39 +4,36 @@ import "fmt"
 
 func trap(height []int) int {
 
-	l, r := 0, 1
-	water := 0
+	l, r := 0, len(height)-1
+	ml, mr := height[l], height[r]
+	trappedWater := 0
 	for {
-		if height[l] > height[r] {
-			r++
-		}
-
-		if height[l] <= height[r] {
-			water += calculateWater(height[l:r])
-			l = r
-			r = l + 1
-		}
-
-		if r == len(height)-1 {
+		if r < l {
 			break
 		}
+
+		ml = max(height[l], ml)
+		mr = max(height[r], mr)
+
+		if ml-height[l] > 0 {
+			trappedWater += ml - height[l]
+		}
+
+		if mr-height[r] > 0 {
+			trappedWater += mr - height[r]
+		}
+
+		if ml <= mr {
+			l++
+		} else {
+			r--
+		}
 	}
 
-	return water
-}
-
-func calculateWater(sub []int) int {
-	subWater := 0
-	height := min(sub[0], sub[len(sub)-1])
-
-	for i := 1; i < len(sub)-2; i++ {
-		subWater += height - sub[i]
-	}
-
-	return subWater
+	return trappedWater
 }
 
 func main() {
-	r := trap([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1})
+	r := trap([]int{4, 2, 0, 3, 2, 5})
 	fmt.Printf("R: %v\n", r)
 }
