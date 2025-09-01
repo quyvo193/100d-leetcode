1f6bd96d48fbeb8188f7d57b93444ccd67008c85
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-01 09:31:21 +0700

    min stack done

--- medium/min-stack.go
+++ medium/min-stack.go
@@ -4,22 +4,31 @@ import "fmt"
 
 type MinStack struct {
 	stack []int
-	min   int
+	min   []int
 }
 
 func Constructor() MinStack {
 	return MinStack{
 		stack: make([]int, 0),
+		min:   make([]int, 0),
 	}
 }
 
 func (this *MinStack) Push(val int) {
-	this.min = min(this.min, val)
 	this.stack = append(this.stack, val)
+
+	if len(this.min) == 0 || val <= this.min[len(this.min)-1] {
+		this.min = append(this.min, val)
+	}
 }
 
 func (this *MinStack) Pop() {
+	pop := this.stack[len(this.stack)-1]
 	this.stack = this.stack[:len(this.stack)-1]
+
+	if pop == this.min[len(this.min)-1] {
+		this.min = this.min[:len(this.min)-1]
+	}
 }
 
 func (this *MinStack) Top() int {
@@ -27,7 +36,7 @@ func (this *MinStack) Top() int {
 }
 
 func (this *MinStack) GetMin() int {
-	return 0
+	return this.min[len(this.min)-1]
 }
 
 func main() {
@@ -36,10 +45,19 @@ func main() {
 	obj.Push(-3)
 	obj.Push(0)
 	obj.Push(-4)
-	fmt.Println("top", obj.Top())
-	obj.Pop()
-	param_3 := obj.Top()
-	param_4 := obj.GetMin()
+	obj.Push(2)
+	fmt.Println("stack: ", obj.stack)
 
-	fmt.Printf("p3: %v, p4: %v\nstack: %v\n", param_3, param_4, obj.stack)
+	obj.Pop()
+	obj.Pop()
+	obj.Pop()
+	obj.Pop()
+	obj.Pop()
+
+	min := obj.GetMin()
+
+	fmt.Printf("min: %v\nstack: %v\n", min, obj.stack)
 }
+
+// 1 -3 0 -4
+// 1 -3 -4
