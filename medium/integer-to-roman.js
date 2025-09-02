a58be7dcbc03da221cdfd19e87b202cd2d9d0be6
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-02 22:49:06 +0700

    integer to roman.js done

--- medium/integer-to-roman.js
+++ medium/integer-to-roman.js
@@ -3,33 +3,46 @@
  * @return {string}
  */
 var intToRoman = function (num) {
-  const map = {
+  const romanMap = {
     1: "I",
+    2: "II",
+    3: "III",
+    4: "IV",
     5: "V",
+    6: "VI",
+    7: "VII",
+    8: "VIII",
+    9: "IX",
     10: "X",
+    20: "XX",
+    30: "XXX",
+    40: "XL",
     50: "L",
+    60: "LX",
+    70: "LXX",
+    80: "LXXX",
+    90: "XC",
     100: "C",
+    200: "CC",
+    300: "CCC",
+    400: "CD",
     500: "D",
+    600: "DC",
+    700: "DCC",
+    800: "DCCC",
+    900: "CM",
     1000: "M",
+    2000: "MM",
+    3000: "MMM",
   };
-
-  const romanVal = [1000, 500, 100, 50, 10, 5, 1];
-  const roman = [];
-  let i = 0;
+  let roman = "";
+  let i = 1;
 
   while (num) {
-    let x = num / romanVal[i];
-
-    if (x === 0) {
-      i++;
-    } else {
-      for (let j = 0; j < x; j++) {
-        roman.push(map[romanVal[j]]);
-      }
-      num -= x * romanVal[i];
-      i++;
-    }
+    roman = (romanMap[(num % 10) * i] || "") + roman;
+    num = Math.floor(num / 10);
+    i *= 10;
   }
 
-  return roman.join("");
+  return roman;
 };
