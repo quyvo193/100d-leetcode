e451c25a287107b2f55516cbdf787a8fe431522b
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-03 04:02:28 +0700

    daily-temperature upgrade done

--- medium/daily-temperature.go
+++ medium/daily-temperature.go
@@ -2,36 +2,34 @@ package main
 
 import "fmt"
 
+type HigherTemp struct {
+	idx int
+	val int
+}
+
 func dailyTemperatures(temperatures []int) []int {
+	n := len(temperatures)
 	res := make([]int, len(temperatures))
+	stack := []HigherTemp{}
 
-	for i := 0; i < len(temperatures); i++ {
-		if i == 0 || temperatures[i] < temperatures[i-1] {
-			for j := i + 1; j < len(temperatures); j++ {
-				if temperatures[i] < temperatures[j] {
-					res[i] = j - i
-					break
-				}
-			}
-		} else {
-			if i > 0 && res[i-1] == 0 {
-				res[i] = 0
-				continue
-			}
-
-			for j := i + res[i-1] - 1; j < len(temperatures); j++ {
-				if temperatures[i] < temperatures[j] {
-					res[i] = j - i
-					break
-				}
-			}
+	for i := n - 1; i >= 0; i-- {
+		for len(stack) > 0 && stack[len(stack)-1].val <= temperatures[i] {
+			stack = stack[:len(stack)-1]
 		}
 
+		if len(stack) > 0 {
+			res[i] = stack[len(stack)-1].idx - i
+		}
+
+		stack = append(stack, HigherTemp{
+			idx: i,
+			val: temperatures[i],
+		})
 	}
 
 	return res
 }
 
 func main() {
-	fmt.Println("result: ", dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}))
+	fmt.Println("result: ", dailyTemperatures([]int{89, 62, 70, 58, 47, 47, 46, 76, 100, 70}))
 }
