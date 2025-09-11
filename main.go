855047647fbaf8c5b4ab115ff77703ca4850a542
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-11 19:34:32 +0700

    binary search

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"leetcode/medium"
+	"leetcode/hard"
 )
 
 func main() {
-	fmt.Println("r: ", medium.CarFleet(12, []int{10, 8, 0, 5, 3}, []int{2, 4, 1, 1, 3}))
+	fmt.Println("r: ", hard.LargestRectangleArea([]int{2, 4}))
 }
