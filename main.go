cc73f9770bc40a65163f82c31fdea123b3168caa
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-15 00:22:46 +0700

    can eating speed

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	p := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}
-	// fmt.Println("r: ", easy.Search([]int{1}, 2))
-	fmt.Println("r: ", medium.SearchMatrix(p, 11))
+
+	fmt.Println("r: ", medium.MinEatingSpeed([]int{30, 11, 23, 4, 20}, 5))
 }
