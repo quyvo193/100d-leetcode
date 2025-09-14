7d59ff94e5740c7fddd002b1b8e9fd28b8dfe754
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-15 00:59:27 +0700

    find min in rotated sorted array

---
+++ medium/find-min-rotated-sorted-array.go
@@ -0,0 +1,17 @@
+package medium
+
+func FindMin(nums []int) int {
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		m := l + (r-l)/2
+
+		if nums[m] < nums[r] {
+			r = l
+		} else {
+			l = m + 1
+		}
+	}
+
+	return nums[l]
+}
