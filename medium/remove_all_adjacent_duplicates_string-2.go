e315e80195cdf27fa09abc01860c42d76a228862
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-22 16:18:13 +0700

    remove all adjacent duplicatates in string 2 optimal solution

--- medium/remove_all_adjacent_duplicates_string-2.go
+++ medium/remove_all_adjacent_duplicates_string-2.go
@@ -1,26 +1,29 @@
 package medium
 
+type Item struct {
+	Char  byte
+	Count int
+}
+
 func RemoveDuplicates(s string, k int) string {
-	stack := []byte{}
+	stack := []Item{}
+	res := []byte{}
 
 	for i := 0; i < len(s); i++ {
-		count := 1
-
-		for len(stack) > 0 && stack[len(stack)-count] == s[i] {
-			if count == k-1 {
-				stack = stack[:len(stack)-k+1]
-				goto endloop
-			}
-
-			count++
-			if count > len(stack) {
-				break
+		if len(stack) > 0 && stack[len(stack)-1].Char == s[i] && stack[len(stack)-1].Count == k-1 {
+			stack = stack[:len(stack)-k]
+		} else {
+			if stack[len(stack)-1].Char == s[i] {
+				stack = append(stack, Item{Char: s[i], Count: stack[len(stack)-1].Count + 1})
+			} else {
+				stack = append(stack, Item{Char: s[i], Count: 1})
 			}
 		}
-
-		stack = append(stack, s[i])
-	endloop:
 	}
 
-	return string(stack)
+	for _, item := range stack {
+		res = append(res, item.Char)
+	}
+
+	return string(res)
 }
