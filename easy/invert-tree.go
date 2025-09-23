cbd6d06569a72d8dfea5675b7a3c51d80c2af256
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-24 01:05:53 +0700

    invert tree clean solution

--- easy/invert-tree.go
+++ easy/invert-tree.go
@@ -7,21 +7,9 @@ type TreeNode struct {
 }
 
 func InvertTree(root *TreeNode) *TreeNode {
-	var invert func(node *TreeNode)
-	invert = func(node *TreeNode) {
-		if node == nil {
-			return
-		}
-
-		tmp := node.Left
-		node.Left = node.Right
-		node.Right = tmp
-
-		invert(node.Left)
-		invert(node.Right)
+	if root != nil {
+		root.Left, root.Right = InvertTree(root.Right), InvertTree(root.Left)
 	}
-
-	invert(root)
 
 	return root
 }
