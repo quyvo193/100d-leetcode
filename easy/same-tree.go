aca04d77de838d33a6854dd044596a3131e391fa
Quy Vo <quyvo@Quys-MacBook-Air.local>
2025-09-24 02:25:08 +0700

    same tree clean solution

--- easy/same-tree.go
+++ easy/same-tree.go
@@ -1,24 +1,14 @@
 package easy
 
 func IsSameTree(p *TreeNode, q *TreeNode) bool {
-	res := true
-
-	var isSame func(p *TreeNode, q *TreeNode)
-	isSame = func(p *TreeNode, q *TreeNode) {
-		if (q == nil && p == nil) || !res {
-			return
-		}
-
-		if (p == nil && q != nil) || (p != nil && q == nil) || (p.Val != q.Val) {
-			res = false
-			return
-		}
-
-		isSame(p.Left, q.Left)
-		isSame(p.Right, q.Right)
+	if p == nil && q == nil {
+		return true
 	}
-
-	isSame(p, q)
-
-	return res
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return IsSameTree(p.Left, q.Left) && IsSameTree(p.Right, q.Right)
 }
